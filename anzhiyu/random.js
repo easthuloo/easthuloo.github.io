3abf2bdd54f6a46debe5eb030571cc9a8ea21ced
easthuloo <huloo_9@163.com>
2024-08-24 15:56:55 +0800

    Site updated: 2024-08-24 15:56:51

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/96f2062d.html","posts/240824.html","posts/240822.html","posts/16107.html"];function toRandomPost(){
+var posts=["posts/96f2062d.html","posts/240822.html","posts/16107.html","posts/240824.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
